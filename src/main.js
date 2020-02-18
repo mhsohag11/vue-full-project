a5943a12deb7c5ef936f69ea658a556baf5d6f14
mhsohag11 <mhsohag11@gmail.com>
2020-02-18 16:14:40 +0600

    flickering problem solved
    auto signin problem solved

--- src/main.js
+++ src/main.js
@@ -12,26 +12,24 @@ Vue.component('app-alert' , Alert)
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App),
-  created () {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyA0hKn6-P2qQ0vN0GuSLqruVEGKUnPwMRg',
-      authDomain: 'devs-meet.firebaseapp.com',
-      databaseURL: 'https://devs-meet.firebaseio.com',
-      projectId: 'devs-meet',
-      storageBucket: 'devs-meet.appspot.com',
-      messagingSenderId: '1072966140458',
-      appId: '1:1072966140458:web:2609bc503b6ab4dc50f953',
-      measurementId: 'G-1KE775377N'
-    })
-    firebase.auth().onAuthStateChanged( (user) => {
-      if (user) {
-        this.$store.dispatch('autoSignIn' , user )
-      }
-    })
-  },
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: 'AIzaSyA0hKn6-P2qQ0vN0GuSLqruVEGKUnPwMRg',
+  authDomain: 'devs-meet.firebaseapp.com',
+  databaseURL: 'https://devs-meet.firebaseio.com',
+  projectId: 'devs-meet',
+  storageBucket: 'devs-meet.appspot.com',
+  messagingSenderId: '1072966140458',
+  appId: '1:1072966140458:web:2609bc503b6ab4dc50f953',
+  measurementId: 'G-1KE775377N'
+})
+firebase.auth().onAuthStateChanged( (user) => {
+  if (user) {
+    store.dispatch('autoSignIn' , user )
+  } 
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App),
+  }).$mount('#app')  
+})
